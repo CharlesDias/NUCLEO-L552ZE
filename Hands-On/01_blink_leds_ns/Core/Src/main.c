d38ea65a76dc3b787dbad943d59a7d54ba708262
Charles Dias <charlesdias.cd@outlook.com>
2024-02-08 14:27:21 -0300

    feat(01_blink): add the blink LEDs without TrustZone

--- Hands-On/01_blink_leds_ns/Core/Src/main.c
+++ Hands-On/01_blink_leds_ns/Core/Src/main.c
@@ -112,6 +112,15 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    HAL_GPIO_TogglePin(LED_BLUE_GPIO_Port, LED_BLUE_Pin);
+    HAL_Delay(100);
+
+    HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+    HAL_Delay(100);
+
+    HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+    HAL_Delay(100);
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
